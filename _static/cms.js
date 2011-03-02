3fcf8a4d88f9f554c9ca2f1d184f82d083416d52
alvivi <alvivi@gmail.com>
2011-03-02 20:54:44 +0100

    Version 1.0 de la web

--- _static/cms.js
+++ _static/cms.js
@@ -1,224 +1,416 @@
 
-lyahcms = 'http://lyahcms.appspot.com'
+lyahcms_ = 'http://lyahcms.appspot.com'
+lyahcms = 'http://localhost:8080'
 
 
-function postComment (thread, name, url, text) {
-    $.getJSON(lyahcms + '/post?callback=?',
-              { 'name'     : name
-              , 'url'      : url
-              , 'thread'   : thread
-              , 'text'     : text
-              });   
-}
 
-function getComments (thread, cb) {
-    $.getJSON(lyahcms + '/comments?callback=?',
-              { 'thread' : thread  }, cb);
-}
-
-function getCommentsCount (thread, cb) {
-    $.getJSON(lyahcms + '/count?callback=?', { 'thread' : thread},
-        function (data) {
-            var count = data.count || 0;
-            cb(count);
-        });
-}
-
-
-function getThreadId () {
-    if (!getThreadId.id)
-        getThreadId.id = /^.*\/(.*)\.html$/i.exec(location.pathname)[1];
-    return getThreadId.id;
-}
-
-/*
- * Utility
- */
-
-function setCSSBoxShadow (o, s) {
-    $(o).css('-moz-box-shadow', s)
+jQuery.prototype.cssShadow = function (s) {
+    this.css('-moz-box-shadow', s)
         .css('-webkit-box-shadow', s)
         .css('box-shadow', s);
-}
+    return this; 
+};
 
-function setCSSBorderRadius (o, s) {
-    $(o).css('-moz-border-radius', s)
+jQuery.prototype.cssBorderRadius = function (s) {
+    this.css('-moz-border-radius', s)
         .css('border-radius', s);
-}
+    return this; 
+};
 
 
-/*
- * Comment weappers
- */
- 
-function makeCmtWrapper (i, obj) {
-    $(obj).wrap('<div class= "cmt-wrapper" id ="' + getThreadId() + i + '">');
-}
 
-function selectCmtWrapper (obj) {
-    deselectCmtWrapper();
 
-    $(obj).addClass('selected');
-    $(obj).css('background-color', '#dfdfdf');
-    setCSSBoxShadow(obj, '0 0 5px 5px #dfdfdf');
-          
-}
+var CommentsSystem = function () {
+    this.ccounts = {};
+    this.isCCInitialized = false;
+    this.selectedPara = null;
+    
+    this.setup();
+};
 
-function deselectCmtWrapper () {
-    $('.selected').each(function (i) {
-        cs = $(this).children('.comments');
-        cs.slideUp('fast', function () {
-          cs.remove();  
+
+CommentsSystem.prototype.group = function () {
+    return /^.*\/(.*)\.html$/i.exec(location.pathname)[1];
+} ();
+
+CommentsSystem.prototype.select = function (para) {
+    if (this.selectedPara)
+        this.selectedPara.onUnselect();
+    this.selectedPara = para;
+    para.onSelect();
+};
+
+CommentsSystem.prototype.unselect = function () {
+    if (this.selectedPara)
+        this.selectedPara.onUnselect();
+    this.selectedPara = null;
+};
+
+CommentsSystem.prototype.setup = function () {
+    var self = this;
+    $('img').css('position', 'relative').css('z-index', '69');
+    
+    $('.body p').not('.admonition-title').each(function (i) {
+        new Paragraph(self, i, this);
+    });
+};
+
+CommentsSystem.prototype.postComment = function (thread, c, cb)
+{
+    var self = this;
+    var out = { 'group'  : this.group
+              , 'thread' : thread
+              , 'author' : c.author
+              , 'url'    : c.url
+              , 'text'   : c.text
+              };
+              
+    $.getJSON(lyahcms + '/post?callback=?', out, function(data) {
+        if (data.success)
+            self.updateCommentsCount();
+        cb(data.success);
+    });
+};
+
+CommentsSystem.prototype.updateCommentsCount = function (cb) {
+    var self = this;
+    $.getJSON(lyahcms + '/count?callback=?', { 'group' : this.group},
+        function (data) {
+            if (data.success)
+                self.ccounts = data.count;
+            else
+                self.ccounts = {};
+            if (cb) cb();
         });
-        $(this).removeClass('selected');
-        setCmtWrapperDefaultStyle(this);        
-    });
-}
-
-function isCmtWrapperSelected (obj) {
-    return $(obj).hasClass('selected');
-}
-
-
-function setCmtWrapperDefaultStyle (obj) {
-    $(obj).css('background-color', 'transparent');
-    setCSSBoxShadow(obj, '0 0 0 0 transparent');
-}
-
-function setCmtWrapperHoverStyle (obj) {
-    $(obj).css('position', 'relative').css('background-color', '#f5d96e');
-    setCSSBorderRadius(obj, '7px');
-    setCSSBoxShadow(obj, '0 0 5px 5px #f5d96e');
-}
-
-function addCmtWrapperLabel (obj) {
-    removeCmtWrapperLabel();
-    if(isCmtWrapperSelected(obj)) return;
-    $(obj).prepend('<div id="cmt-label"><a>Comentarios</a></div>');
+};
     
-    var id = $(obj).attr('id');
-    var label = $('#cmt-label');
-    var link = $('#cmt-label a');
-        
-    label.css('position', 'absolute')
-         .css('display', 'none')
-         .css('top', ($(obj).height() / 2 - label.height() / 2) + 'px')
-         .css('left', ($(obj).width() / 2 - label.width() / 2) + 'px')
-         .css('background-color', 'rgba(230,230,230,0.9)')
-         .css('border', 'solid 1px rgba(40,40,40,0.6)')
-         .css('border-bottom', 'solid 1px rgba(40,40,40,0.75)')
-         .css('border-top', 'solid 1px rgba(40,40,40,0.3)')
-         .css('cursor', 'pointer')
-         .css('padding', '0 5px 0 5px');
-    setCSSBoxShadow(label, '0 0 2px 2px rgba(60,60,60,0.25)');
-    setCSSBorderRadius(label, '10px');
-    link.css('color', '#222')
-        .css('text-decoration', 'none')
-        .css('text-shadow','0px 1px 1px rgba(255,255,255,1)');
-           
-    label.fadeIn('slow');
-    
-    getCommentsCount(id, function (c) {
-        var str = (c == 0) ? 'Sin comentarios' :
-                  ((c == 1) ? '1 Comentario' : c + ' Comentarios');
-        link.text(str);
-    });
-    
-    link.click(function (e) {
-        e.preventDefault();
-        selectCmtWrapper(obj);
-        link.text('Cargando...');
-        getComments(id, addCmtList);
-    });
-}
-
-function showComment(cmt) {
-    title = cmt.url ? '<a href="' + cmt.url + '">' + cmt.name + '</a>'
-                    : cmt.name;
-    return '<div class="comment"><h5>' + title + '</h5><h6>' + cmt.date
-           + '</h6><p>' + cmt.text + '</p></div>';
-}
-
-function addCmtList(data) {
-    $('#cmt-label').fadeOut();
-    var wrapper = $('.selected');
-    
-    var cmts = '<div class="comments">';
-    for (c in data.cs) {
-        cmts += showComment(data.cs[c]);   
+CommentsSystem.prototype.withCommentCount = function (thread, cb) {
+    var self = this;
+    if (!this.isCCInitialized) {
+        this.updateCommentsCount(function () {
+            self.isCCInitialized = true;
+            cb(self.ccounts[thread] || 0);
+        });
     }
-    cmts += '</div>';
-    
-    wrapper.append(cmts);
-    addCmtForm();
-    var cmtslist = wrapper.children('.comments');
-    var wpad = wrapper.width() / 16;
-    cmtslist.hide().slideDown('fast');
+    else 
+        cb(this.ccounts[thread] || 0);
+};
 
-    cmtslist.css('margin', '0 ' + wpad + 'px 0 ' + wpad + 'px')
-            .css('position', 'static');
-    $('.comments h5').css('color', '#555')
-                     .css('margin-bottom', '0');
-    $('.comments a').css('color', '#555');
-    $('.comments h6').css('font-size', 'x-small')
-                     .css('color', '#555')
-                     .css('line-height', '0');
-    $('.comments p').css('text-indent', 0)
-                    .css('font-size', 'small')
-                    .css('color', '#5A5A5A')
-                    .css('padding-top', '1ex')
-                    .css('padding-left', wpad / 2 + 'px');
-}
-
-function addCmtForm() {
-    var wrapper = $('.selected');
-    var cmtslist = wrapper.children('.comments');
-    var form = '<div class="cmtform"><h4>¡Comentar es gratis!</h4><form><p><input type="text" name="author_name" id="author_name" size="40" tabindex="1"><label id="anlabel" for="author_name">Nombre (obligatorio)</label></br><input type="text" name="author_url" id="author_url" size="40" tabindex="2"><label id="aulabel" for="author_url">Página web (opcional)</label><br/><textarea name="text" id="cmttext" rows="3" cols="60" tabindex="3"></textarea><br/><input type="submit"id="submit" tabindex="4" value="Enviar"></p></form></div>'
-
-    cmtslist.append(form);
-
-    var cmtform = $('.cmtform');
-    cmtform.css('display', 'block')
-           .css('border-top', 'solid 1px #AAA')
-           .css('padding-top', '16px')
-           .css('line-height', '0');
-    $('h4').css('color', '#555');
-    $('#submit').click(function (e) {
-        e.preventDefault();
-        postComment($('.selected').attr('id'), $('#author_name').val(),
-                    $('#author_url').val(), $('#cmttext').val());
-        deselectCmtWrapper();
-    });
-
-}
-
-function removeCmtWrapperLabel () {
-    if($('#cmt-label').length)
-        $('#cmt-label').remove();
-}
-
-function setCmtWrappersPrts () {
-    $('.cmt-wrapper').hover(
-        function () {
-            if(!isCmtWrapperSelected(this)) {            
-                setCmtWrapperHoverStyle(this);
-                addCmtWrapperLabel(this);
-            }
-        },
-        function () {
-            if(!isCmtWrapperSelected(this)) {
-                setCmtWrapperDefaultStyle(this);
-                removeCmtWrapperLabel();
+CommentsSystem.prototype.withComments = function (thread, cb) {
+    $.getJSON(lyahcms + '/comments?callback=?',
+        { 'group' : this.group, 'thread' : thread },
+        function (data) {
+            if(data.success) {
+                var cs = [];
+                $.each(data.cs, function (i, c) { cs.push(new Comment(c)); });
+                cb(cs);
             }
         });
-}
+};
+
+
+
+
+
+var Comment = function (c) {
+    this.author = unescape(c.author);
+    this.url = c.url;
+    this.date = decodeURI(c.date);
+    this.text = unescape(c.text);
+};
+
+Comment.prototype.attach = function (wrapper) {
+    var title = this.url ? '<a href="' + this.url + '">' + this.author + '</a>'
+                         : this.author;
+    var str = '<div class="comment"><h5>' + title + '</h5><h6>' + this.date
+            + '</h6><pre>' + this.text + '</pre></div>';    
+    wrapper.append(str);
+};
+
+
+
+var CommentsBox = function (cs, p) {
+    this.comments = [];
+    this.cs = cs;
+    this.para = p;
+    this.wrapper = null;
+    this.form = new Form(cs, this);
+    
+    this.attach();
+};
+                
+CommentsBox.prototype.attach = function () {
+    this.para.contents.append('<div class="comments"></div>');
+    this.wrapper = this.para.contents.find('.comments');
+    this.wrapper.css('padding', '3ex 6ex 1ex 6ex');
+    this.wrapper.hide();
+};
+
+CommentsBox.prototype.loadComments = function (cb) {
+    var self = this;
+    this.cs.withComments(this.para.thread, function (comments) {
+        self.comments = comments;
+        $.each(comments, function (i, c) { c.attach(self.wrapper); });
+        self.setupStyle();
+        if (cb) cb();
+    });
+};
+
+CommentsBox.prototype.onSubmit = function () {
+    var self = this;
+    this.cs.postComment(this.para.thread, this.form.getComment(), function (){
+       self.cs.unselect();
+    });
+};
+
+CommentsBox.prototype.setupStyle = function() {
+    this.wrapper.find('.comment').css('background-color', '#E8E8E8')
+                                 .cssBorderRadius('10px')
+                                 .cssShadow('0 0 3px 3px #E8E8E8')
+                                 .css('margin-top', '2ex');
+    this.wrapper.find('h5').css('color', '#555')      
+                           .css('margin-bottom', '0');
+    this.wrapper.find('a').css('color', '#555');
+    this.wrapper.find('h6').css('font-size', 'x-small')
+                           .css('color', '#555')
+                           .css('line-height', '0');
+    this.wrapper.find('pre').css('text-indent', 0)
+                            .css('font-size', 'small')
+                            .css('font-family', 'Consolas, "Courier New", monospace')
+                            .css('color', '#5A5A5A')
+                            .css('line-height', '2ex')
+                            .css('padding', '2ex 0 0 6ex');
+};
+
+CommentsBox.prototype.show = function () {
+    var self = this;
+    this.loadComments(function () {
+        self.form.show();
+        self.wrapper.slideDown('fast');        
+    });
+};
+
+CommentsBox.prototype.hide = function () {
+    var self = this;
+    this.wrapper.slideUp('fast', function () {
+        self.form.hide();        
+        self.wrapper.empty();
+    });    
+};
+
+
+
+
+
+var Form = function (cs, cb) {
+    this.cs = cs;
+    this.csBox = cb;
+    this.form = null;
+    this.isSubmited = false;
+};
+
+Form.prototype.src = '<div class="cmtform"><h4>¡Comentar es gratis!</h4><form><p><input type="text" name="author_name" id="author_name" size="40" tabindex="1"><label id="anlabel" for="author_name">Nombre (obligatorio)</label></p><p><input type="text" name="author_url" id="author_url" size="40" tabindex="2"><label id="aulabel" for="author_url">Página web (opcional, incluir protocolo (\"http:\\\\\", \"https:\\\\\"))</label><p/><p><textarea name="text" id="cmttext" rows="7" cols="60" tabindex="3"></textarea><p/><p><input type="submit"id="submit" tabindex="4" value="Enviar"></p></form></div>';
+
+Form.prototype.getComment = function ()
+{
+    return new Comment({ author : escape(this.form.find('#author_name').val())
+                       , url    : encodeURI(this.form.find('#author_url').val())
+                       , text   : escape(this.form.find('#cmttext').val())
+                       });
+};    
+
+Form.prototype.hide = function ()
+{
+    this.form.remove();
+    this.form = null;
+};
+
+Form.prototype.setup = function ()
+{   
+    var self = this;
+    var submit = this.form.find('#submit');
+    this.isSubmited = false;
+    this.form.css('display', 'block')
+             .css('border-top', 'solid 1px #AAA')
+             .css('margin-top', '2ex');
+    this.form.find('h4').css('color', '#555')
+                        .css('text-indent', '0');
+    this.form.find('p').css('text-indent', '0')
+                       .css('font-size', 'small')    
+                       .css('color', '#555')
+                       .css('padding', '0')
+                       .css('margin', '0');
+    this.form.find('label').css('padding-left', '1ex');
+    submit.css('margin-top', '1ex')
+    submit.click(function (e) {
+        e.preventDefault();
+        if(!self.isSubmited) {
+            self.isSubmited = true;
+            self.csBox.onSubmit();
+        }
+    });
+};
+
+Form.prototype.show = function ()
+{
+    this.csBox.wrapper.append(this.src);
+    this.form = this.csBox.wrapper.find('.cmtform');
+    this.setup();
+};
+
+
+
+var Paragraph = function (cs, thread, contents) {
+    this.contents = $(contents);
+    this.cs = cs;
+    this.isHover = false;
+    this.thread = thread;
+    this.toggle = new Toggle(cs, this);
+    this.csBox = new CommentsBox(cs, this);
+    
+    this.attach();
+    this.setDefaultStyle();
+};
+
+
+Paragraph.prototype.attach = function () {
+    var self = this;
+    
+    this.contents.wrap('<div class="par-wrapper">');
+    this.contents = this.contents.parent();
+    
+    this.contents.hover( function () { self.onHoverIn();  }
+                       , function () { self.onHoverOut(); }
+                       );
+};
+
+Paragraph.prototype.isSelected = function() {
+    var thread = this.cs.selectedPara ? this.cs.selectedPara.thread : -1;
+    return this.thread === thread;
+};
+
+Paragraph.prototype.onHoverIn = function () {
+    if(!this.isSelected()) {    
+        this.isHover = true;
+        this.setBackground('#f5d96e');
+        this.toggle.show();
+    }
+};
+
+Paragraph.prototype.onHoverOut = function () {
+    if(!this.isSelected()) {
+        this.isHover = false;
+        this.setBackground('transparent');
+        this.toggle.hide();
+    }
+};
+
+Paragraph.prototype.onSelect = function () {
+    this.setBackground('#dfdfdf');
+    this.toggle.hide();
+    this.csBox.show();
+};
+
+Paragraph.prototype.onUnselect = function () {
+    this.setBackground('transparent');
+    this.csBox.hide();
+};
+
+Paragraph.prototype.setBackground = function (color) {
+    this.contents.css('background-color', color)
+                 .cssShadow('0 0 5px 5px ' + color);
+};
+
+Paragraph.prototype.setDefaultStyle = function () {
+     this.contents.css('position', 'relative')
+                  .cssBorderRadius('10px');
+};
+
+
+
+
+
+
+
+var Toggle = function (cs, p) {
+    this.cs = cs;
+    this.link = null;
+    this.para = p;
+    this.wrapper = null;
+    this.isAttached = false;
+    this.isClicked = false;
+};
+
+
+Toggle.prototype.setLabel = function () {
+    var self = this;
+    this.cs.withCommentCount(this.para.thread, function(c) {
+        var str = (c === 0) ? 'Sin comentarios' :
+                  (c === 1) ? '1 Comentario'    : c + ' Comentarios';
+        self.link.text(str);
+    });
+};
+
+Toggle.prototype.setup = function () {
+    var self = this;
+    var src = '<div class="toggle"><a>Cargando...</a></div>';
+    this.para.contents.prepend(src);
+    this.wrapper = this.para.contents.find('.toggle');
+    this.link = this.para.contents.find('.toggle a');
+    this.setupStyle();
+
+    this.link.click(function (e) {
+        e.preventDefault();
+        if (!self.isClicked) {
+            self.isClicked = true;
+            self.cs.select(self.para);
+            self.link.text('Cargando...');
+        }
+    });
+};
+
+Toggle.prototype.setupStyle = function () {
+    var x = this.para.contents.width()  / 2 - this.link.width() / 2;
+    var y = this.para.contents.height() / 2 - this.wrapper.height();
+        
+    this.wrapper.css('position', 'absolute')
+                .css('display', 'none')
+                .css('background-color', 'rgba(230,230,230,0.9)')
+                .css('border', 'solid 1px rgba(40,40,40,0.6)')
+                .css('border-bottom', 'solid 1px rgba(40,40,40,0.75)')
+                .css('border-top', 'solid 1px rgba(40,40,40,0.3)')
+                .css('cursor', 'pointer')
+                .css('padding', '0 5px 0 5px')
+                .cssShadow('0 0 2px 2px rgba(60,60,60,0.25)')
+                .cssBorderRadius('10px')
+                .css('top', y + 'px')
+                .css('left', x + 'px'); 
+    this.link.css('color', '#222')
+             .css('text-decoration', 'none')
+             .css('text-shadow','0px 1px 1px rgba(255,255,255,1)');
+};
+
+Toggle.prototype.show = function () {
+    var self = this;
+    if (!this.isAttached) {
+        this.setup();
+        this.isAttached = true;
+    }
+    this.setLabel();
+    this.wrapper.fadeIn('slow', function () {
+        if(!self.para.isHover)
+            self.hide();
+    });
+};
+
+Toggle.prototype.hide = function () {
+    this.wrapper.hide();
+    this.isClicked = false;
+};
+
+
+
 
 $(window).load(function () {
-    console.log("starting...");
-    console.log(getThreadId());
-    $('img').css('position', 'relative').css('z-index', '69');
-    $('.body p').not('.first').each(function (i) {
-        makeCmtWrapper(i, this);
-    });
-    setCmtWrappersPrts();
+    CommentsSystem.prototype.instance = new CommentsSystem();
 });
